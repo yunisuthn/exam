f4472a9f0a5ae5c400188dbb6a976ded91e7a553
yunisuthn <yunis.thn@gmail.com>
2019-07-22 16:11:00 +0300

    ajout

---
+++ client/src/users/Dashboard/AfficheProfil.js
@@ -0,0 +1,81 @@
+
+import React, { Component } from 'react';
+import axios from 'axios';
+
+import { Link } from 'react-router-dom';
+import { Button, FormGroup, FormControl, FormLabel } from "react-bootstrap";
+
+export default class AfficheProfil extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = { profil: [] };
+
+    }
+    componentDidMount() {
+        axios.get(`http://localhost:8080/userArticle/${localStorage.id}`)
+            .then(response => {
+                console.log('user-article ==== ', response)
+                this.setState({ profil: response.data });
+            })
+            .catch(function (error) {
+                console.log(error);
+            })
+
+        
+
+    }
+
+    ajout = event => {
+        window.location = "/dashboard";
+      }
+    
+    liste() {
+        return <table className="table">
+                    <thead>
+                        <tr>
+                            <th>NOM</th>
+                            <th>PRIX</th>
+                            <th>DESCRIPTION</th>
+                            <th>PHOTO</th>
+                            <th>ACTION</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            (this.state.profil.length > 0) ? (this.state.profil.map((obj) => {
+                                
+                                return <tr key={obj._id}>
+                                    <td>{obj.nom}</td>
+                                    <td>{obj.prix}</td>
+                                    <td>{obj.description}</td>
+                                    <td>
+                                        <img width="150px" height="50px" src={'http://localhost:8080/photos/'+obj.photo_profil} alt="pdp" />
+                                    </td>
+                                    <td>
+                                        <Link to={"/edit/"+ obj._id} className="btn btn-primary">Edit</Link>
+                                    </td>
+                                    {console.log(obj)}
+                                </tr>
+
+                            })) : ('')
+                        }
+                    </tbody>
+                </table>
+    }
+    render() {
+        return (
+            <div className='app1'>
+                {this.liste()}
+                      <Button
+                          onClick={this.ajout}
+                          type="submit"
+                      >
+                          Retour
+                      </Button>
+
+
+            </div>
+        );
+    }
+}
---
+++ router/userController.js
@@ -0,0 +1,18 @@
+const account = require('../controllers/lib');
+
+module.exports = function (app) {
+    app.post('/login',account.login)
+    .post('/signup',account.signup)
+
+    .post('/particulier',account.createPart)
+    .get('/particulier',account.getPart)
+
+
+    .post('/article', account.createArt)
+    .get('/article', account.findArt)
+    .get('/article/:profilId', account.findOneArt)
+    .post('/article/:noteId', account.update)
+    // .delete('/article/:noteId', account.delete)
+    .get('/userArticle/:noteId', account.findUserArt)
+    .get('/photos/:photo_profil', account.lireImage)
+}
